80cd3162cff6382d53815ccc0535d1eb38eb8517
cortezle <00029117@uca.edu.sv>
2020-06-09 18:04:02 -0600

    se actualizó a un solo button

--- src/main/java/com/uca/capas/controller/MainController.java
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -56,7 +56,7 @@ public class MainController {
 		return mav;
 	}
 	
-	@RequestMapping(value="/mostrarEstudiante",method=RequestMethod.POST)
+	@RequestMapping(value="/mostrarEstudiante",method=RequestMethod.POST,params="action=buscar")
 	public ModelAndView findOne(@RequestParam(value="Codigo") int id) {
 		ModelAndView mav = new ModelAndView();
 		Estudiante estudiante = null;
@@ -68,6 +68,31 @@ public class MainController {
 		mav.addObject("estudiante", estudiante);
 		mav.setViewName("estudiante");
 		return mav;
+	}
+	
+	@RequestMapping(value="/mostrarEstudiante",method=RequestMethod.POST,params="action=borrar")
+	public ModelAndView BorrarOne(@RequestParam(value="Codigo") int id) {
+		ModelAndView mav = new ModelAndView();
+		List<Estudiante> estudiantes=null;
+		try {
+			estudianteService.delete(id);
+			estudiantes = estudianteService.findAll();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		mav.addObject("estudiantes", estudiantes);
+		mav.setViewName("main");
+		return mav;
+	}
+	
+	@RequestMapping(value="/mostrarEstudiante",method=RequestMethod.POST,params="action=editar")
+	public ModelAndView editar(@RequestParam(value="Codigo")int codigoEstudiante) {
+		ModelAndView mav = new ModelAndView();
+		Estudiante estudiante = estudianteService.findOne(codigoEstudiante);
+		mav.setViewName("editarEstudiante");
+		mav.addObject("estudiante",estudiante);
+		return mav;
+		
 	}
 	
 	@RequestMapping("/save")
@@ -145,18 +170,5 @@ public class MainController {
 		mav.addObject("estudiantes", estudiantes);
 		return mav;
 	}*/
-	
-	@RequestMapping("/editar")
-	public ModelAndView editar(@RequestParam(value="codigo")int codigoEstudiante) {
-		ModelAndView mav = new ModelAndView();
-		Estudiante estudiante = estudianteService.findOne(codigoEstudiante);
-		mav.setViewName("editarEstudiante");
-		mav.addObject("estudiante",estudiante);
-		return mav;
-		
-	}
-	
-	
-
 }
 
